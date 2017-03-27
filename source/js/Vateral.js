97507a26f885b541b76c3d1ae0915ddce205fc18
moumao <1195358569@qq.com>
2017-03-28 00:07:05 +0800

    Daily update
    日常更新 layout部分样式的更新

--- source/js/Vateral.js
+++ source/js/Vateral.js
@@ -1,3 +1,94 @@
+$(function () {
+    function pageready() {
+        //禁止x轴滚动条
+        $("body").css({
+            "overflow-x":"hidden"
+        });
+
+        //滚动条滚动时获取左侧导航栏以及背景的top
+        $(window).scroll(function () {
+            var top=$(window).scrollTop();
+            $("#menu-outer").css({
+                "top":top
+            });
+            $(".background").css({
+             "top":top
+             });
+        });
+
+        //动态获取整个页面高度，设置给导航栏和背景
+        var height=$(window).height();
+        $(window).resize(function () {
+            var height=$(window).height();
+            $("#menu-outer").css({
+                "height":height
+            });
+             $(".background").css({
+             "height":height
+             });
+        });
+        $("#menu-outer").css({
+            "height":height
+        });
+         $(".background").css({
+         "height":height
+         });
+    }
+    pageready()
+});
+
+//导航栏弹出绑定事件
+$(function () {
+    function nanup() {
+        $(".nav-btn").click(function () {
+            $("#menu-outer").animate({
+                "left":0
+            },500);
+            $("#bottom-outer").animate({
+                "left":"300px"
+            },500);
+            $(".background").css({
+                "display":"block"
+            });
+            $(this).rotate({animateTo: 180});
+            $(this).animate({
+                "left":"330px"
+            },500);
+            $("body").css({
+                "overflow-y":"hidden"
+            });
+            return false;
+        });
+    }
+    nanup()
+});
+
+//导航栏收起绑定事件
+$(function () {
+    function navdown() {
+        $(".background").click(function () {
+            $("#menu-outer").animate({
+                "left":"-300px"
+            },500);
+            $("#bottom-outer").animate({
+                "left":0
+            },500);
+            $(".background").css({
+                "display":"none"
+            });
+            $(".nav-btn").animate({
+                "left":"30px"
+            },500);
+            $(".nav-btn").rotate({animateTo: 0});
+            /*$(".left-list-out ul li").slideUp();
+            $(".left-list-down").rotate({animateTo: 0});*/
+        });
+    }
+    navdown();
+});
+
+
+
 //foot固定在底部
 $(function(){
     function footerPosition(){
@@ -14,3 +105,4 @@ $(function(){
     footerPosition();
     $(window).resize(footerPosition);
 });
+
