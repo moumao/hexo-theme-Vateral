b79db528ec131dba8027298c457d6ed717c36b7f
moumao <1195358569@qq.com>
2017-04-19 22:50:29 +0800

    updata friends page
    增加友情链接，引入google material图标

--- source/js/Vateral.js
+++ source/js/Vateral.js
@@ -25,8 +25,8 @@ $(function () {
             $('.button-collapse').sideNav({
                     menuWidth: 240, // Default is 240
                     edge: 'left', // Choose the horizontal origin
-                    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                    draggable: true // Choose whether you can drag to open on touch screens
+                    closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                    draggable: false // Choose whether you can drag to open on touch screens
                 }
             );
             $(".nav-btn").click(function () {
