8664d9b51c358b05786d048d9257ca77a5f2b89f
moumao <1195358569@qq.com>
2017-05-03 11:20:46 +0800

    Add photo

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@
     });
 
     //压缩，合并 js
-    gulp.task('minifyjs',function() {
+    gulp.task('uglify',function() {
         return gulp.src('./source/js/Vateral.js')      //需要操作的文件
             //.pipe(concat('main.js'))    //合并所有js到main.js
             //.pipe(gulp.dest('js'))       //输出到文件夹
@@ -36,5 +36,5 @@
 
 　　//默认命令，在cmd中输入gulp后，执行的就是这个任务(压缩js需要在检查js之后操作)
     gulp.task('default', [
-        'minifycss','minifyjs'
+        'minifycss','uglify'
     ]);
